e2c9c475a7e42849585be7da11c8173f1958e56e
Moonbeom KWON <MBKWON@WMP-2016090004.local>
2017-06-15 11:46:20 +0900

    made styb but it has some errors

--- SwiftList/main.swift
+++ SwiftList/main.swift
@@ -10,3 +10,50 @@ import Foundation
 
 print("Hello, World!")
 
+
+
+indirect enum myList <A> {
+    case Nil
+    case Cons(head: A, tail: myList)
+}
+
+
+extension myList {
+    
+    func sum<A: ExpressibleByIntegerLiteral>(ints: myList) -> A {
+        
+        switch self {
+        case .Nil:
+            return 0
+            
+        case .Cons(head: let a, tail: let b):
+            return a + sum(ints: b)
+            
+        default:
+            return 0
+        }
+    }
+    
+    func product<A: ExpressibleByFloatLiteral>(ds: myList) -> A {
+        
+        switch self {
+        case .Nil:
+            return 1.0
+            
+        case .Cons(head: let a, tail: let b):
+            return a * product(ds: b)
+            
+        default:
+            return 1.0
+        }
+    }
+    
+    func apply<A>(values: A...) -> myList<A>? {
+        
+        if values.count == 0 {
+            return .Nil    
+        } else {
+            .Cons(values.first, apply(values: values.dropFirst()))
+        }
+    }
+}
