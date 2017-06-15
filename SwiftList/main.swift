34a8956c96ccdd08c91a45ac6a358c4a1e145702
Moonbeom KWON <MBKWON@WMP-2016090004.local>
2017-06-15 10:14:18 +0900

    init project

---
+++ SwiftList/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftList
+//
+//  Created by Moonbeom KWON on 2017. 6. 15..
+//  Copyright © 2017년 mbkyle. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
