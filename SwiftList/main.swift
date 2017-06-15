8680e4de6c7d7ddcf9a6c18870b4fc096e9c8a7e
Moonbeom KWON <MBKWON@WMP-2016090004.local>
2017-06-15 15:31:56 +0900

    made a numericType but it has some errors

--- SwiftList/main.swift
+++ SwiftList/main.swift
@@ -10,50 +10,92 @@ import Foundation
 
 print("Hello, World!")
 
+protocol NumericType {
+    static func +(lhs: Self, rhs: Self) -> Self
+    static func -(lhs: Self, rhs: Self) -> Self
+    static func *(lhs: Self, rhs: Self) -> Self
+    static func /(lhs: Self, rhs: Self) -> Self
+    static func %(lhs: Self, rhs: Self) -> Self
+    init(_ v: Int)
+}
+
+extension Double : NumericType {}
+extension Float  : NumericType {}
+extension Int    : NumericType {}
+extension Int8   : NumericType {}
+extension Int16  : NumericType {}
+extension Int32  : NumericType {}
+extension Int64  : NumericType {}
+extension UInt   : NumericType {}
+extension UInt8  : NumericType {}
+extension UInt16 : NumericType {}
+extension UInt32 : NumericType {}
+extension UInt64 : NumericType {}
 
 
-indirect enum myList <A> {
+
+indirect enum myList <A: NumericType> {
     case Nil
-    case Cons(head: A, tail: myList)
+    case Cons(head: A, tail: myList<A>)
 }
 
 
 extension myList {
     
-    func sum<A: ExpressibleByIntegerLiteral>(ints: myList) -> A {
+    func sum<T: NumericType>(ints: myList<T>) -> T {
         
         switch self {
         case .Nil:
-            return 0
+            return T(0)
             
         case .Cons(head: let a, tail: let b):
-            return a + sum(ints: b)
+            return a + sum(ints: b) as! T
             
         default:
-            return 0
+            return T(0)
         }
     }
     
-    func product<A: ExpressibleByFloatLiteral>(ds: myList) -> A {
+    func product<T: NumericType>(ds: myList<T>) -> T {
         
         switch self {
         case .Nil:
-            return 1.0
+            return T(1)
             
         case .Cons(head: let a, tail: let b):
-            return a * product(ds: b)
+            return a * product(ds: b) as! T
             
         default:
-            return 1.0
+            return T(1)
         }
     }
     
-    func apply<A>(values: A...) -> myList<A>? {
+    func apply<T: NumericType>(values: T...) -> myList<T> {
         
         if values.count == 0 {
-            return .Nil    
+            return .Nil
         } else {
-            .Cons(values.first, apply(values: values.dropFirst()))
+            if let first = values.first {
+                return .Cons(first, apply(values: Array<T>(values.dropFirst())) )
+            }
+            
+            return .Nil
+        }
+    }
+    
+    func apply<T: NumericType>(values: [T]) -> myList<T> {
+        
+        if values.count == 0 {
+            return .Nil
+        } else {
+            if let first = values.first {
+                return .Cons(first, apply(values: Array<T>(values.dropFirst())) )
+            }
+            
+            return .Nil
         }
     }
 }
+
+
+
